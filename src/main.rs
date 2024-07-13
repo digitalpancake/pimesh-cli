03ad1d144e1801eb9ddf9d5e2c803997bf0a43a5
Jules White <juleswhite@127.0.0.1>
2024-07-13 22:39:55 +0100

    Add basic cli args and user command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(
+    version,
+    about,
+    long_about = "CLI for interacting with a PiMesh System"
+)]
+struct Cli {
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    User,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.debug {
+        1 => println!("Debug Low"),
+        2 => println!("Debug High"),
+        _ => {},
+    }
+
+    match &cli.command {
+        Some(Commands::User) => {
+            println!("User Stuff")
+        },
+        None => {},
+    }
+
 }
